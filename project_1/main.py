87260bbe2cdc1c553173c91869d5c6a7cfe1827b
Dylan Crocker <dylan.corker@live.com>
2016-01-31 18:52:41 -0700

    Added project 1 start.

---
+++ project_1/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# This file is the main script for the simulations.
+# The program simulates the flow of people leaving a stadium and heading
+# to their desired locations. The simulation is based on Cellular Automata.
+
+import numpy as np
+
+
+class Grid(object):
+    """This class is used to represent the simulation grid."""
+    pass
+
+
+class Cell(object):
+    """This class is used to represent a grid cell."""
+    pass
+
+
+class Human(object):
+    """This class represents a human traveling in the grid."""
+    pass
+
+
+if __name__ == "__main__":
+    print("Begin Simulation")
+    print("End Simulation")
